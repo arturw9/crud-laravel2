2a5a9d7fde082f973c2617486b0d49c791339040
arturw9 <arturw9@hotmail.com>
2022-05-24 11:49:16 -0300

    Conexão com o banco de dados, controller e migrations

--- routes/web.php
+++ routes/web.php
@@ -13,36 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $nome = "Artur";
-    $idade = 23;
-    $profissao = "Desenvolvedor Fullstack Jr";
+use App\Http\Controllers\EventController;
 
-    $arr = [10,20,30,40,50];
-
-    $nomes = ["Matheus", "Maria", "Joao", "Saulo"];
-
-    return view('welcome',
-        [
-            'nome' => $nome,
-            'idade' => $idade,
-            'profissao'=>$profissao,
-            'arr' => $arr,
-            'nomes' => $nomes
-        ]);
-});
+Route::get('/',[EventController::class, 'index'] );
+Route::get('/events/create',[EventController::class, 'create'] );
 
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('product',['id'=>$id]);
 });
